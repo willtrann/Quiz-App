ffb94000c0a9b5b8fd4a0572535fef38537161a2
William Tran <wtra7541@uni.sydney.edu.au>
2026-05-28 23:32:12 +1000

    updated frontend for vercel:

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    process.env.REACT_APP_API_URL ||
+    "http://localhost:5000/api",
 });
 
 API.interceptors.request.use((req) => {
