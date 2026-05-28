414449329fd28d7542148f80d421d9b939c937c2
William Tran <wtra7541@uni.sydney.edu.au>
2026-05-28 23:27:00 +1000

    Configure backend for Vercel

--- backend/server.js
+++ backend/server.js
@@ -48,6 +48,10 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+  });
+}
+
+module.exports = app;
